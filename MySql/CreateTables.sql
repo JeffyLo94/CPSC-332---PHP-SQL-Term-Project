925b617f4aae42a790a542db8ffc28e8144734f4
JeffyLo94 <jeffrey.lo94@yahoo.com>
2018-12-18 11:38:12 -0800

    SQL Tables

--- MySql/CreateTables.sql
+++ MySql/CreateTables.sql
@@ -12,7 +12,80 @@ CREATE TABLE PROFESSOR
 	Professor_Sex enum('M', 'F') not null,
 	Professor_Title enum('Dr.', 'Mr.', 'Ms.', 'Mrs.') not null,
 	Professor_Salary numeric(6) not null,
+  Professor_Degree varchar(25) not null,
 	primary key(Professor_SSN)
 );
 
-/* Example SQL Table -> more needs to be added*/
+CREATE TABLE DEPARTMENT
+(
+	Department_Number numeric(5) not null,
+	Department_Name varchar(25) not null,
+	Department_Phone numeric(10) not null,
+	Department_Location varchar(10) not null,
+	Department_Chair_Person numeric(9) not null,
+	primary key(Department_Number, Department_Chair_Person),
+	foreign key(Department_Chair_Person) references PROFESSOR(Professor_SSN)
+);
+
+CREATE TABLE COURSE
+(
+	Course_Department_Number numeric(5) not null,
+	Course_Number varchar(10) not null,
+	Course_Title varchar(50) not null,
+	Course_Textbook varchar(50) not null,
+	Course_Units tinyint not null,
+	primary key(Course_Number),
+	foreign key(Course_Department_Number) references DEPARTMENT(Department_Number)
+);
+
+/* NOTE: this wasn't in the relational model, but is in the ER Schema */
+CREATE TABLE PREREQUISITE
+(
+	Parent_Course varchar(10) not null,
+	Child_Course  varchar(10) not null,
+	primary key(Parent_Course, Child_Course),
+	foreign key(Parent_Course) references COURSE(Course_Number),
+	foreign key(Child_Course) references COURSE(Course_Number)
+);
+
+CREATE TABLE STUDENT
+(
+	Student_CWID numeric(10) not null,
+	Student_FName varchar(20) not null,
+	Student_LName varchar(20) not null,
+	Student_Address varchar(60) not null,
+	Student_Phone numeric(10) not null,
+	Major numeric(5) not null,
+  Minor numeric(5) not null,
+	primary key(Student_CWID),
+	foreign key(Major) references DEPARTMENT(Department_Number)
+);
+
+
+CREATE TABLE SECTION
+(
+	Section_Number numeric(5) not null,
+	Section_Course_Number varchar(10) not null,
+	Section_Professor_SSN varchar(9) not null, /* Don't need a seperate PSSN teaches, since we already assigned a professor */
+	Classroom varchar(10) not null,
+	Meeting_Days char(7) not null,
+	Beg_Time varchar(10) not null,
+	End_Time varchar(10) not null,
+	Seats numeric(3) not null,
+	primary key(Section_Number, Section_Course_Number, Section_Professor_SSN),
+	foreign key(Section_Course_Number) references COURSE(Course_Number),
+	foreign key(Section_Professor_SSN) references PROFESSOR(Professor_SSN)
+);
+
+/* Didn't have any course enrollment? I've moved "Course_number_has" into this enrollment model */
+CREATE TABLE ENROLLMENT
+(
+	Enrollment_CWID numeric(10) not null,
+	Enrollment_Section_Number numeric(10) not null,
+	Enrollment_Course_Number varchar(10) not null,
+	Grade enum('A','A-','B+','B','B-','C+','C','C-','D+','D','D-','F') not null,
+	primary key(Enrollment_Section_Number, Enrollment_CWID, Enrollment_Course_Number),
+	foreign key(Enrollment_Section_Number) references SECTION(Section_Course_Number),
+	foreign key(Enrollment_CWID) references STUDENT(Student_CWID),
+	foreign key(Enrollment_Course_Number) references COURSE(Course_Number)
+);
